9ca0e73d0b7fd13eaf870ae3182e9edd73982cad
HALI12_hat <hussainaliabdulrasool004@gmail.com>
2026-08-10 11:35:45 +0300

    Fixed an issue where the if would always throw the error instead of checking first, also removed authorization in he header we forgot to not iinclude it

--- src/services/heroServices.js
+++ src/services/heroServices.js
@@ -2,15 +2,14 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}`
 
 const allHeroes = async()=>{
     try {
-        const res = await fetch(`${BASE_URL}/heroes`,{
-        method: 'Get',
-        headers: {Authorization: `Bearer ${localStorage.getItem('token')}`},
-    })
+        const res = await fetch(`${BASE_URL}/heroes`)
     const heroesData = await res.json()
     
     
-    if (heroesData.err) return console.log(heroesData.err) 
+    if (heroesData.err) {
+        return console.log(heroesData.err) 
         throw new Error(heroesData.err)
+    }
    
     return heroesData
     } catch (err) {
@@ -22,18 +21,18 @@ const allHeroes = async()=>{
 
 const singleHero = async (heroId)=>{
 try {
-    const res = await fetch(`${BASE_URL}/heroes/:${heroId}`, {
-        headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
-    })
+    const res = await fetch(`${BASE_URL}/heroes/${heroId}`)
 
     const heroData = await res.json()
 
-    if (heroData.err) return console.log(heroData.err) 
+    if (heroData.err) {
+        return console.log(heroData.err) 
         throw new Error(err)
+    } 
 
     return heroData
-} catch (error) {
-    throw new Error(error)
+} catch (err) {
+    throw new Error(err)
 }
 
 }
