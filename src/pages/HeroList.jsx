ad10a5a9209b21ea029f2a4f5f79f97e9a8875a7
Ali <Alialaliwat21@gmail.com>
2026-08-12 19:46:40 +0300

    new change

--- src/pages/HeroList.jsx
+++ src/pages/HeroList.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import { Link } from 'react-router'
-import Loading from '../components/Loading'
 
 
 const HeroList = ({ heroes, isLoading }) => {
 
-    if (isLoading) {
-    return <Loading />
-    }
+
+  const [showLegacy, setShowLegacy] = useState(false)
+
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
 
 
 
